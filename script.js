3eefec6df42b0001523af7a428a702983c07b48c
ROCKBY777 <rbayu2505@gmail.com>
2024-11-02 21:00:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
 function toggleReadMore(dotsId, moreId, btnId) {
-  var dots = document.getElementById(dotsId);
-  var moreText = document.getElementById(moreId);
-  var btnText = document.getElementById(btnId);
-
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.textContent = "Read More";
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.textContent = "Read Less";
-    moreText.style.display = "inline";
+    var dots = document.getElementById(dotsId);
+    var moreText = document.getElementById(moreId);
+    var btnText = document.getElementById(btnId);
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.textContent = "Read More";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.textContent = "Read Less";
+      moreText.style.display = "inline";
+    }
   }
-}
+  
+function closeNavbar() {
+    const navbarCollapse = document.getElementById("navbarNav");
+    const bsCollapse = new bootstrap.Collapse(navbarCollapse, {
+      toggle: false
+    });
+    bsCollapse.hide();
+  }
+  const themeToggleBtn = document.getElementById("theme-toggle");
 
+  themeToggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
 
- function closeNavbar() {
-        const navbarCollapse = document.getElementById("navbarNav");
-        const bsCollapse = new bootstrap.Collapse(navbarCollapse, {
-          toggle: false
-        });
-        bsCollapse.hide();
-      }
+    // Change button text based on current theme
+    if (document.body.classList.contains("dark-mode")) {
+      themeToggleBtn.textContent = "Light Mode";
+      themeToggleBtn.classList.remove("btn-outline-dark");
+      themeToggleBtn.classList.add("btn-outline-light");
+    } else {
+      themeToggleBtn.textContent = "Dark Mode";
+      themeToggleBtn.classList.remove("btn-outline-light");
+      themeToggleBtn.classList.add("btn-outline-dark");
+    }
+  });
