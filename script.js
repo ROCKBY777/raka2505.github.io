f834a04acec49dc2348367b3027058a603ecf534
ROCKBY777 <rbayu2505@gmail.com>
2024-11-02 20:48:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,19 @@
-function toggleReadMore() {function toggleReadMore() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("readMoreBtn");
-  
-    if (dots.style.display === "none") {
-        dots.style.display = "inline";
-        btnText.innerHTML = "Read More"; 
-        moreText.style.display = "none";
-    } else {
-        dots.style.display = "none";
-        btnText.innerHTML = "Read Less"; 
-        moreText.style.display = "inline";
-    }
-  }
-  var dots = document.getElementById("dots");
-  var moreText = document.getElementById("more");
-  var btnText = document.getElementById("readMoreBtn");
+function toggleReadMore(dotsId, moreId, btnId) {
+  var dots = document.getElementById(dotsId);
+  var moreText = document.getElementById(moreId);
+  var btnText = document.getElementById(btnId);
 
   if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read More"; 
-      moreText.style.display = "none";
+    dots.style.display = "inline";
+    btnText.textContent = "Read More";
+    moreText.style.display = "none";
   } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read Less"; 
-      moreText.style.display = "inline";
+    dots.style.display = "none";
+    btnText.textContent = "Read Less";
+    moreText.style.display = "inline";
   }
 }
+
  function closeNavbar() {
         const navbarCollapse = document.getElementById("navbarNav");
         const bsCollapse = new bootstrap.Collapse(navbarCollapse, {
