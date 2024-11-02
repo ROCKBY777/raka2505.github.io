e0ce5af1fb98450844efc55853b19b2423834003
ROCKBY777 <rbayu2505@gmail.com>
2024-11-02 17:25:55 +0700

    Add files via upload

---
+++ Tugas Praktikum/script.js
@@ -0,0 +1,29 @@
+function toggleReadMore() {function toggleReadMore() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("readMoreBtn");
+  
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read More"; 
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read Less"; 
+        moreText.style.display = "inline";
+    }
+  }
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("readMoreBtn");
+
+  if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read More"; 
+      moreText.style.display = "none";
+  } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read Less"; 
+      moreText.style.display = "inline";
+  }
+}
